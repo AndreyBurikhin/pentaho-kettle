d1b66480ce6536be0c7d8af04dfb4a60e7fc78c9
AndreyBurikhin <Andrey_Burikhin@epam.com>
2016-03-02 17:14:46 +0400

    [PDI-13761] - ETL Metadata Injection + Row (de)normalizer - quickfix

--- engine/src/org/pentaho/di/trans/steps/normaliser/NormaliserMetaInjection.java
+++ engine/src/org/pentaho/di/trans/steps/normaliser/NormaliserMetaInjection.java
@@ -113,16 +113,17 @@ public class NormaliserMetaInjection implements StepMetaInjectionInterface {
       }
     }
 
-    // Pass the grid to the step metadata
-    //
-    //CHECKSTYLE:Indentation:OFF
-    meta.allocate( normaliserFields.size() );
-    for ( int i = 0; i < normaliserFields.size(); i++ ) {
-      meta.getFieldName()[i] = normaliserFields.get( i ).getName();
-      meta.getFieldValue()[i] = normaliserFields.get( i ).getValue();
-      meta.getFieldNorm()[i] = normaliserFields.get( i ).getNorm();
+    if ( !normaliserFields.isEmpty() ) {
+      // Pass the grid to the step metadata
+      //
+      // CHECKSTYLE:Indentation:OFF
+      meta.allocate( normaliserFields.size() );
+      for ( int i = 0; i < normaliserFields.size(); i++ ) {
+        meta.getFieldName()[i] = normaliserFields.get( i ).getName();
+        meta.getFieldValue()[i] = normaliserFields.get( i ).getValue();
+        meta.getFieldNorm()[i] = normaliserFields.get( i ).getNorm();
+      }
     }
-
   }
 
   public List<StepInjectionMetaEntry> extractStepMetadataEntries() throws KettleException {
